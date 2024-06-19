3375d6f6f3de194e5fa470fb5509f957e5ed814e
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-06-19 11:51:42 +0300

    feat: middleware to redirect user from /currency to currency/startDate/endDate

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
+import { serverDayjs } from "@/src/config/dayjs"
 
 import {
     getLocaleFromRequest,
@@ -6,6 +7,10 @@ import {
     pathnameHasLocale,
 } from "@/src/middlewares"
 import { SELECTED_LOCALE_KEY } from "@/src/config/constants"
+import {
+    DEFAULT_START_DATE,
+    EXCHANGE_INTERVAL_SIZE,
+} from "@/src/currency/constants"
 
 export async function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname
@@ -28,10 +33,19 @@ export async function middleware(request: NextRequest) {
         newPath = `${persistedLocale}/${breakpoint}/${pathname}`
     }
 
+    if (newPath.endsWith("currency")) {
+        const startDate = DEFAULT_START_DATE
+        const endDate = startDate.add(EXCHANGE_INTERVAL_SIZE, "day")
+        const formattedStartDate = startDate.format("YYYY-MM-DD")
+        const formattedEndDate = endDate.format("YYYY-MM-DD")
+
+        newPath = `${newPath}/${formattedStartDate}/${formattedEndDate}`
+    }
+
     return NextResponse.redirect(new URL(newPath, request.url))
 }
 
 export const config = {
-    matcher: ["/((?!_next).*)"],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
 
